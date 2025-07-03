a264956dc1fa97f01f8c548a6aa65381ac20aba2
FrozenFox2 <ffrozennfox@gmail.com>
2025-07-03 15:39:52 +1200

    Fixing Chopsticks collision with bowl

--- really-noodle/ChopstickScripts/chopsticks.gd
+++ really-noodle/ChopstickScripts/chopsticks.gd
@@ -1,14 +1,16 @@
-extends Node2D
+extends CharacterBody2D
 
-@export var max_speed := 400.0
-var velocity := Vector2.ZERO
+@export var max_speed := 200.0
 
 func _physics_process(delta):
 	var mouse_position = get_viewport().get_mouse_position()
 	var to_mouse = mouse_position - global_position
 	var distance = to_mouse.length()
 
-	if distance > 1: 
+	if distance > 1:
 		var direction = to_mouse.normalized()
 		velocity = direction * min(distance / delta, max_speed)
-		global_position += velocity * delta
+	else:
+		velocity = Vector2.ZERO
+
+	move_and_slide()
